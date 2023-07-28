40ce503f57a44dab6f460c6f87a741da03f9ff07
Ivan Lozic <ivanlozic995@gmail.com>
2023-07-28 20:19:50 +0200

    Refactored

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-const fs = require('fs')
+const fs = require('fs').promises // Use fs.promises for async file operations
 const express = require('express')
 const app = express()
+const port = 5000
 
-app.use(express.json());
+const dataPath = `${__dirname}/data`
+const menuFilePath = `${dataPath}/menu.json`
+const ordersFilePath = `${dataPath}/orders.json`
+
+app.use(express.json())
+
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
@@ -10,47 +16,63 @@ app.use((req, res, next) => {
   next()
 })
 
-const menu = JSON.parse(fs.readFileSync(`${__dirname}/data/menu.json`))
-const orders = JSON.parse(fs.readFileSync(`${__dirname}/data/orders.json`))
-
-const getMenu = (req, res) => {
-  res.status(200).json(menu)
+const readJsonFile = async (filePath) => {
+  try {
+    const fileData = await fs.readFile(filePath)
+    return JSON.parse(fileData)
+  } catch (error) {
+    console.error(`Error reading from ${filePath}:`, error)
+    throw error
+  }
 }
 
-const createOrder = (req, res) => {
-  // Check if orders array is empty
-  console.log('Received request to create an order:', req.body);
-  const newId = orders.length > 0 ? orders[orders.length - 1].id + 1 : 1
-  const newOrder = Object.assign({ id: newId }, req.body)
+const writeJsonFile = async (filePath, data) => {
+  try {
+    await fs.writeFile(filePath, JSON.stringify(data, null, 2))
+  } catch (error) {
+    console.error(`Error writing to ${filePath}:`, error)
+    throw error
+  }
+}
 
-  orders.push(newOrder)
+const getMenu = async (req, res) => {
+  try {
+    const menu = await readJsonFile(menuFilePath)
+    res.status(200).json(menu)
+  } catch (error) {
+    res.status(500).json({
+      status: 'error',
+      message: 'Failed to fetch the menu.'
+    })
+  }
+}
 
-  fs.writeFile(
-    `${__dirname}/data/orders.json`,
-    JSON.stringify(orders),
-    (err) => {
-      if (err) {
-        console.error('Error writing to orders.json:', err)
-        res.status(500).json({
-          status: 'error',
-          message: 'Failed to create the order.'
-        })
-      } else {
-        res.status(201).json({
-          status: 'success',
-          data: {
-            order: newOrder
-          }
-        })
+const createOrder = async (req, res) => {
+  try {
+    const orders = await readJsonFile(ordersFilePath)
+    const newId = orders.length > 0 ? orders[orders.length - 1].id + 1 : 1
+    const newOrder = { id: newId, ...req.body }
+
+    orders.push(newOrder)
+    await writeJsonFile(ordersFilePath, orders)
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        order: newOrder
       }
-    }
-  )
+    })
+  } catch (error) {
+    res.status(500).json({
+      status: 'error',
+      message: 'Failed to create the order.'
+    })
+  }
 }
 
 app.route('/api/menu').get(getMenu)
 app.route('/api/orders').post(createOrder)
 
-const port = 5000
 app.listen(port, () => {
   console.log('Server is listening')
 })
