a8c98df5007b464b650c39c88e344d8b8f679e85
Ivan Lozic <ivanlozic995@gmail.com>
2023-10-02 16:34:19 -0400

    routes

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
 
 app.use(cors())
 
-app.use('/', routes)
+app.use(express.static(__dirname + '/',routes));
 
 app.listen(port, () => {
   console.log('Server is listening')
--- controllers/menuController.js
+++ controllers/menuController.js
@@ -14,6 +14,7 @@ const getMenu = async (req, res) => {
   }
 };
 
+
 module.exports = {
   getMenu,
 };
