8d985386d7fd853f9057e4b8faf890ec5f852060
Ivan Lozic <ivanlozic995@gmail.com>
2023-10-03 10:58:06 -0400

    a

--- controllers/menuController.js
+++ controllers/menuController.js
@@ -1,20 +1,21 @@
-const { readJsonFile } = require('../utills/fileUtils');
+const { readJsonFile } = require('../utills/fileUtils')
 
 const getMenu = async (req, res) => {
   try {
-    const dataPath = `${__dirname}/db`;
-    const menuFilePath = `${dataPath}/menu.json`;
-    const menu = await readJsonFile(menuFilePath);
-    res.status(200).json(menu);
+    const dataPath = `${__dirname}/db`
+    const menuFilePath = `${dataPath}/menu.json`
+    const menu = await readJsonFile(menuFilePath)
+    res.status(200).json(menu)
   } catch (error) {
-    res.status(500).json({
-      status: 'error',
-      message: 'Failed to fetch the menu.',
-    });
+    console.error('Error fetching menu:', error),
+      res.status(500).json({
+        status: 'error',
+        message: 'Failed to fetch the menu.'
+      })
   }
-};
+}
 
 
 module.exports = {
-  getMenu,
-};
+  getMenu
+}
