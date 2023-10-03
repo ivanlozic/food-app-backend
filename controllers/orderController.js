cc2890eebf4a290d4a1ac772470f482153918c52
Ivan Lozic <ivanlozic995@gmail.com>
2023-10-03 11:22:49 -0400

    get orders

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -26,6 +26,29 @@ const createOrder = async (req, res) => {
   }
 };
 
+
+const getOrders = async (req, res) => {
+  try {
+    const dataPath = `${__dirname}/db`;
+    const ordersFilePath = `${dataPath}/orders.json`;
+
+    const orders = await readJsonFile(ordersFilePath);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        orders: orders,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: 'error',
+      message: 'Failed to retrieve orders.',
+    });
+  }
+};
+
 module.exports = {
   createOrder,
+  getOrders,
 };
