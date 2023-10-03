aa485ad6a4f089de4c4ed3f6d7d4f98ded5ac019
Ivan Lozic <ivanlozic995@gmail.com>
2023-10-03 10:48:45 -0400

    Cors

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const routes = require('./routes/routes')
 app.use(express.json())
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+  res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
   next()
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
 
 app.use(cors())
 
-app.use(express.static(__dirname + '/',routes));
+app.use('/', routes)
 
 app.listen(port, () => {
   console.log('Server is listening')
