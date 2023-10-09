740e044f2bbc8beb2510d0626aae431dafc7127d
Ivan Lozic <ivanlozic995@gmail.com>
2023-10-09 10:41:03 -0400

    f

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mongoose = require('./db/db')
 const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000
-
+app.use(express.json())
 app.use(cors())
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
@@ -13,9 +13,6 @@ app.use(function (req, res, next) {
 })
 
 const routes = require('./routes/routes')
-
-app.use(express.json())
-
 app.use('/', routes)
 
 app.listen(port, () => {
