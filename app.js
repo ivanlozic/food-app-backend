114976979a552c97dc45a0b85a0f717bd50d6ecc
Ivan Lozic <ivanlozic995@gmail.com>
2023-10-09 10:35:43 -0400

    e

--- app.js
+++ app.js
@@ -2,16 +2,13 @@ const express = require('express')
 const mongoose = require('./db/db')
 const cors = require('cors')
 const app = express()
-const port = 'https://fluffy-jay-peplum.cyclic.cloud' || 5000
+const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE')
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type,Accept, x-client-key, x-client-token, x-client-secret, Authorization'
-  )
+  res.header('Access-Control-Allow-Headers', '*')
   next()
 })
 
