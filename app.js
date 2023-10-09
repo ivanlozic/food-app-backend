ef6ca8790e589438480fb4a399e179a8aeedf329
Ivan Lozic <ivanlozic995@gmail.com>
2023-10-09 10:03:39 -0400

    cors

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const routes = require('./routes/routes')
 app.use(express.json())
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+  res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
   next()
