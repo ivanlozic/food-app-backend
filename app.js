0af5afc3d2fd3e65433d4cc2ad1a4010918095df
Ivan Lozic <ivanlozic995@gmail.com>
2023-10-09 10:28:46 -0400

    d

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const express = require('express')
 const mongoose = require('./db/db')
-const cors = require('cors');
+const cors = require('cors')
 const app = express()
-const port = 'https://fluffy-jay-peplum.cyclic.cloud' || 5000;
+const port = 'https://fluffy-jay-peplum.cyclic.cloud' || 5000
 
-const corsOptions ={
-  origin:'http://localhost:3000', 
-  credentials:true,            
-  optionSuccessStatus:200
-}
-
-app.use(cors(corsOptions));
+app.use(cors())
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type,Accept, x-client-key, x-client-token, x-client-secret, Authorization'
+  )
+  next()
+})
 
 const routes = require('./routes/routes')
 
