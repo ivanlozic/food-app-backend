5a49a60166843e5e6fa45a07cd43e503bb3b5ee0
Ivan Lozic <ivanlozic995@gmail.com>
2023-10-12 00:31:26 -0400

    cors

--- app.js
+++ app.js
@@ -3,12 +3,17 @@ const mongoose = require('./db/db')
 const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000
+
 app.use(express.json())
 app.use(cors())
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE')
-  res.header('Access-Control-Allow-Headers', '*')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+
   next()
 })
 
